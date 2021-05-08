e73aa390af69e45edf012fa5b81d55480c0bbfaa
mikereil80 <mreilly4@stevens.edu>
2021-05-07 22:14:15 -0400

    Updated movies.js according to Database Requirements Doc

---
+++ data/movies.js
@@ -0,0 +1,87 @@
+const mongoCollections = require('../config/mongoCollections');
+const movies = mongoCollections.movies;
+const axios = require("axios");
+
+const API_KEY = f800afc56a41eeb52666b5b8657cea5e;
+
+let { ObjectId } = require('mongodb');
+
+async function getPopularMovies(){
+    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    return data; // this will be the array of shows objects
+}
+
+function GetTrendingMovies(){
+    /* Send a Request to the movie database api for top movies (https://developers.themoviedb.org/3/movies/get-popular-movies) 
+        For each movie it returns in the array, check if our database has that movie:
+            If it doesn't, then add it where _id in mongo is the tmdbID
+        Return an array of the tmdbIDs */
+
+    const moviesCollection = await movies();
+    let idArray = [];
+
+    const movies = getPopularMovies();
+
+    movies.forEach((show) => {
+        moviesCollection.findOne({tmdbID: show.id}).then((movie) => {
+            if(!movie){
+                let newMovie = {
+                    "tmdbID": show.id,
+                    "reviews": [],
+                    "averageRating": 0
+                };
+                moviesCollection.insert(newMovie, function (err){
+                    if(err){
+                        throw "Movie not inserted successfully";
+                    }
+                    let _id=newMovie._id;
+                    idArray.push(_id);
+                });
+            }
+            else{
+                idArray.push(movie._id);
+            }
+        });
+    });
+    return idArray;
+};
+
+function GetMovieByID(id){
+    // Returns the movie document with the id
+
+    // Error checking for id
+    if(id === undefined || id === null){
+        throw 'No id parameter is given to the GetMovieByID(id) function.';
+    }
+    if(typeof id !== 'string'){
+        throw 'Input id in GetMovieByID(id) is not of type string.';
+    }
+    if(id.length == 0){
+        throw 'Input id in GetMovieByID(id) length is 0, empty string.';
+    }
+    if(id.replace(/\s/g, '').length == 0) {
+        throw 'Input id in GetMovieByID(id) is only empty spaces.';
+    }
+
+    let parsedId = ObjectId(id);
+
+    if(!(parsedId instanceof ObjectId)){
+        throw 'Input id in GetMovieByID(id) is not an instance of an ObjectId.';
+    }
+
+    const moviesCollection = await movies();
+    const movie = await moviesCollection.findOne({ _id: parsedId });
+    if(!movie){
+        throw 'Movie not found.';
+    }
+    else{
+        let final = movie;
+        final._id=movie._id.toString();
+        return final;
+    }
+}
+
+module.exports = {
+    GetTrendingMovies,
+    GetMovieByID
+}
